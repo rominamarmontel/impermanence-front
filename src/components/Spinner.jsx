dfedf5f0bea9d4b08e48de9d38e7165a2ed53a58
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-06-10 16:21:47 +0200

    react-spinner installed

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,6 +1,18 @@
-const Spinner = () => {
+// const spinner =
+//   'https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/m3/react-lists/spinner.gif'
+
+// import ReactLoading from 'react-loading';
+
+// import { useState, CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import './Spinner.css'
+
+function Spinner() {
   return (
-    <div>Spinner</div>
+    <div className="Spinner">
+      <ClipLoader type="spin" className='spin' color={'gray'} height={'1%'} width={'1%'}
+        style={{ display: 'flex' }} />
+    </div>
   )
 }
 
