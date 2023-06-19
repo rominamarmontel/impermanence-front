70978f81b6427e537809937518aa5c6b07d40dce
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-06-19 21:33:56 +0200

    updated

--- src/pages/FilmPage/FilmPageEn.jsx
+++ src/pages/FilmPage/FilmPageEn.jsx
@@ -22,17 +22,16 @@ const FilmPageEn = () => {
     })
   }, []);
 
-  useEffect(() => {
-    if (films) {
-      films.forEach((film) => {
-        console.log(film.english[0])
-      });
-    }
-  }, [films]);
+  // useEffect(() => {
+  //   if (films) {
+  //     films.forEach((film) => {
+  //       console.log(film.english[0])
+  //     });
+  //   }
+  // }, [films]);
 
   const groupedFilms = films ? films.reduce((result, film) => {
     const category = film.english[0]?.category;
-    console.log(category)
     if (category) {
       if (result[category]) {
         result[category].push(film);
