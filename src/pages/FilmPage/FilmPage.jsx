f44266a3cd859c3bb338213c43e976edbecf429a
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-07-19 13:16:00 +0200

    loading=lazy add

--- src/pages/FilmPage/FilmPage.jsx
+++ src/pages/FilmPage/FilmPage.jsx
@@ -84,23 +84,26 @@ const FilmPage = () => {
                   {films.map((film) => (
                     <div className='FilmPage-content' key={film._id}>
                       <Link to={`/films/${film._id}`}>
-                        <div className='FilmPage-position' style={{ display: 'block', margin: '0 auto', overflow: 'hidden', cursor: 'pointer', }}>
-                          <div >
-                            <picture >
-                              <source
-                                media="(max-width: 1350px)"
-                                srcSet={`${film.thumbnailImages[0].replace('/upload/', '/upload/w_577/')} 577w`}
-                              />
-                              <source
-                                media="(min-width: 1351px)"
-                                srcSet={`${film.thumbnailImages[0].replace('/upload/', '/upload/w_400/')} 400w`}
-                              />
+                        <div className='FilmPage-position' style={{ display: 'block', margin: '0 auto', overflow: 'hidden', cursor: 'pointer' }}>
+                          <div>
+                            <picture>
+                              <source media="(max-width: 1350px)" srcSet={`${film.thumbnailImages[0].replace('/upload/', '/upload/w_577/')} 577w`} />
+                              <source media="(min-width: 1351px)" srcSet={`${film.thumbnailImages[0].replace('/upload/', '/upload/w_400/')} 400w`} />
                               <img
                                 src={film.thumbnailImages[0]}
                                 alt={film.title}
                                 style={{
-                                  quality: 10, aspectRatio: '16/9', objectFit: 'cover', transform: 'scale(1.1)', objectPosition: '100% 100%', transitionDuration: '0.5s', width: '100%', height: 'auto', marginBottom: 12
+                                  quality: 10,
+                                  aspectRatio: '16/9',
+                                  objectFit: 'cover',
+                                  transform: 'scale(1.1)',
+                                  objectPosition: '100% 100%',
+                                  transitionDuration: '0.5s',
+                                  width: '100%',
+                                  height: 'auto',
+                                  marginBottom: 12
                                 }}
+                                loading="lazy"
                               />
                             </picture>
                           </div>
