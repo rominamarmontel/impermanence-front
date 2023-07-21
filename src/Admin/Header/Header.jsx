009439b8e2b7a22df7a054a402466960cd943b7d
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-07-21 14:15:35 +0200

    Header.jsx modified

--- src/Admin/Header/Header.jsx
+++ src/Admin/Header/Header.jsx
@@ -1,7 +1,4 @@
 import "./header.scss";
-// import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
-// import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
-
 
 const Header = () => {
 
@@ -12,19 +9,6 @@ const Header = () => {
           <div className="item">
             Admin Panel
           </div>
-          {/* <div className="item">
-            <LanguageOutlinedIcon className="icon" />
-          </div>
-          <div className="item">
-            <ListOutlinedIcon className="icon" />
-          </div>
-          <div className="item">
-            <img
-              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
-              className="avatar"
-            />
-          </div> */}
         </div>
       </div>
     </div>
