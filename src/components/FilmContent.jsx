0a20d7ce544cdd74abe9757e90add5674e148d33
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-08-11 15:58:54 +0200

    css modified

--- src/components/FilmContent.jsx
+++ src/components/FilmContent.jsx
@@ -12,26 +12,12 @@ const FilmContent = ({ film }) => {
   return (
     <div>
       <picture>
-        {/* <source media="(min-width: 1351px)" srcSet={`${film.thumbnailImages[0].replace('/upload/', '/upload/w_400/')} 400w`} />
-        <source media="(max-width: 992px)" srcSet={`${film.thumbnailImages[0].replace('/upload/', '/upload/w_942/')} 942w`} />
-        <source media="(max-width: 414px)" srcSet={`${film.thumbnailImages[0].replace('/upload/', '/upload/w_320/')} 320w`} /> */}
         <img
           src={film.thumbnailImages[0]}
           alt={film.title}
           srcSet={`${mobileImage} 320w, ${pcImage} 400w, ${tabletteImage} 942w`}
           sizes="(max-width: 414px) 320px, (min-width: 1351px) 400px, (max-width: 992px) 942px"
           className='film-image'
-        // style={{
-        //   quality: 10,
-        //   aspectRatio: '16/9',
-        //   objectFit: 'cover',
-        //   transform: 'scale(1.1)',
-        //   objectPosition: '100% 100%',
-        //   transitionDuration: '0.5s',
-        //   width: '100%',
-        //   height: 'auto',
-        //   marginBottom: 12
-        // }}
         />
       </picture>
     </div>
