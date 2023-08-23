74d86f5653a067f531918b499d3667121909a938
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-08-23 21:16:07 +0200

    try

--- src/components/FilmContent.jsx
+++ src/components/FilmContent.jsx
@@ -5,18 +5,17 @@ const FilmContent = ({ film }) => {
   if (!film || !film.thumbnailImages || !film.thumbnailImages[0]) {
     return <div>No Thumbnail Available</div>;
   }
-  const pcImage = film.thumbnailImages[0].replace('/upload/', '/upload/w_400/')
-  const tabletteImage = film.thumbnailImages[0].replace('/upload/', '/upload/w_942/')
-  // const mobileImage = film.thumbnailImages[0].replace('/upload/', '/upload/w_320/')
+  const thumbnailImage = film.thumbnailImages[0];
+  // const pcImage = film.thumbnailImages[0].replace('/upload/', '/upload/w_400/')
+  // const tabletteImage = film.thumbnailImages[0].replace('/upload/', '/upload/w_942/')
 
   return (
     <div>
       <picture>
         <img
-          src={film.thumbnailImages[0]}
+          src={thumbnailImage}
           alt={film.title}
-          // srcSet={`${mobileImage} 320w, ${pcImage} 400w, ${tabletteImage} 942w`}
-          srcSet={`${pcImage} 400w, ${tabletteImage} 942w`}
+          // srcSet={`${pcImage} 400w, ${tabletteImage} 942w`}
           sizes="(min-width: 1351px) 400px, (min-width: 992px) 600px, (max-width: 992px) 942px, 400px"
           className='film-image'
         />
--- src/service/service.jsx
+++ src/service/service.jsx
@@ -4,6 +4,7 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 const myApi = axios.create({
   baseURL: BACKEND_URL,
+  timeout: 10000,
 })
 
 myApi.interceptors.request.use(
